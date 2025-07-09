f56a66a4387146d4b0e64e425cfe644599706265
Maryam Qadeer <maryam.qadeer@nightingaletech.net>
2025-07-09 17:26:40 +0500

    build fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { getListPage } from "@/lib/contentParser";
 import CallToAction from "@/partials/CallToAction";
 import FeatureSection from "@/partials/FeatureSection";
 import HowItWorks from "@/partials/HowItWorks";
 import SeoMeta from "@/partials/SeoMeta";
 
 const Home = () => {
-  const pricingData = getListPage("sections/pricing.md").frontmatter;
 
   return (
     <>
